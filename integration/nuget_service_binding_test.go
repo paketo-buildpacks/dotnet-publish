62e4622c241ca537f178a78f528ae3a97870ff83
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-06-22 18:36:00 +0000

    Utilize occam.Serve matcher

--- integration/nuget_service_binding_test.go
+++ integration/nuget_service_binding_test.go
@@ -56,7 +56,7 @@ func testNugetConfig(t *testing.T, context spec.G, it spec.S) {
 					case "/FindPackagesById()":
 						w.WriteHeader(http.StatusOK)
 					default:
-						t.Fatal(fmt.Sprintf("unknown path: %s", req.URL.Path))
+						t.Fatalf("unknown path: %s", req.URL.Path)
 					}
 				}))
 
